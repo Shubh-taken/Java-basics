f66944c8bd2ab255d37064359edddb4421dc1389
Shubh-taken <itsyourboyshubh@gmail.com>
2026-07-18 01:11:36 +0530

    basics02-learned few condition statements in Java

---
+++ condition.java
@@ -0,0 +1,46 @@
+/*import java.util.*;
+
+public class condition  {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
+        sc.close();
+
+    }
+
+}
+
+import java.util.Scanner;
+
+public class condition {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if (age > 18) {
+            System.out.println("eligible for voting!!!");
+
+        } else {
+            System.out.println("not eligible");
+
+        }
+    }
+
+}*/
+
+import java.util.Scanner;
+
+public class condition {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if (x % 2 == 0) {
+            System.out.println("even number");
+        } else {
+            System.out.println("odd num");
+            sc.close();
+        }
+    }
+}
--- firstclass.java
+++ firstclass.java
@@ -1,35 +1,37 @@
-/*class firstclass {
-    public static void main(String args[]) {
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
-    }
-}
-
-class firstclass {
-    public static void main(String args[]) {
-        // Variables
-        int a = 24;
-        int b = 39;
-        int sum = a + b;
-        int diff = b - a;
-        System.out.println(sum);
-        System.out.println(diff);
-    }
-}*/
-
-/**
- * firstclass
+/*
+ * class firstclass {
+ * public static void main(String args[]) {
+ * System.out.println("*");
+ * System.out.println("**");
+ * System.out.println("***");
+ * System.out.println("****");
+ * }
+ * }
+ * 
+ * class firstclass {
+ * public static void main(String args[]) {
+ * // Variables
+ * int a = 24;
+ * int b = 39;
+ * int sum = a + b;
+ * int diff = b - a;
+ * System.out.println(sum);
+ * System.out.println(diff);
+ * }
+ * }
+ * 
+ * 
+ * 
+ * import java.util.*;
+ * 
+ * public class firstclass {
+ * public static void main(String[] args) {
+ * Scanner sc = new Scanner(System.in);
+ * String name = sc.next();
+ * System.out.println(name);
+ * 
+ * sc.close();
+ * }
+ * 
+ * }
  */
-
-import java.util.*;
-
-public class firstclass {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String name = sc.next();
-        System.out.println(name);
-    }
-
-}
