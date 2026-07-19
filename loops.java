9efffb4fbfa8ead892b689c91bba5f543759a449
Shubh-taken <itsyourboyshubh@gmail.com>
2026-07-19 17:47:06 +0530

    learning loops in java

--- loops.java
+++ loops.java
@@ -1,8 +1,28 @@
 public class loops {
+   public static void main(String args[]) {
+// // counter++ => counter = counter + 1
+// for (int counter = 0; counter < 11; counter++) {
+//     System.out.print(counter + " ");
+// }
+
+// for (int i = 0; i < 11; i++) {
+//     System.out.println(i);
+// }
+
+public class loops {
     public static void main(String args[]) {
-        for (int counter = 0; counter < 3; counter = counter + 1) {
-            System.out.println("Hello Master");
+        // int i = 0;
+        // while (i < 11) {
+        //     System.out.println(i);
+        //     i++;
+
+        // }
+        int i = 0;
+        do{
+            System.out.println(i);
+             
         }
+
     }
 
 }
